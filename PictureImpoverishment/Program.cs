213eeaae9c58a55cc75a6e3fca4714fad9cba453
Dominik Riedl <dominik.riedl96@web.de>
2023-08-12 11:56:55 +0200

    v1.3 // multiple changes and additions

--- PictureImpoverishment/Program.cs
+++ PictureImpoverishment/Program.cs
@@ -1,120 +1,271 @@
-﻿using System;
+﻿using NAudio.Wave;
+using System;
+using System.Collections.Generic;
 using System.Drawing;
+using System.IO;
+using System.Threading;
 
 namespace PictureImpoverishment
 {
     internal class Program
     {
+        public const int timer = 2000;
+        public const int timerFiles = 200;
+        public static readonly string directoryPath = @"IMG\Unmod\";
+        public static readonly string audioFilePathSuccess = @"Audio\successAudio.mp3";
+        public static readonly string audioFilePath = @"Audio\";
+        public static readonly string loadFilePath = @"IMG\Unmod\";
+        public static readonly string saveFilePath = @"IMG\Mod\";
+        static bool stopMusic = false;
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Start? (Y/N)");
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("       --------------------------------------------------------------------------------------------------");
+            Console.WriteLine("       |                     --> PICTURE IMPOVERISHMENT V1.3 Release Build <--                          |");
+            Console.WriteLine("       --------------------------------------------------------------------------------------------------");
+            Console.WriteLine();
+            Console.WriteLine("Press any button to continue.");
+            Console.ReadKey();
+            Console.Clear();
+            Console.ForegroundColor= ConsoleColor.White;
 
-            if (Console.ReadKey().Key == ConsoleKey.Y)
-            {
-                Console.Clear();
-                LoadPicture();
-            }
-        }
-        public static void LoadPicture()
-        {
             try
             {
-                // Load the image
-                using (Bitmap imgload = new Bitmap("C:/Users/domrie01/Documents/PictureImpoverishment/PictureImpoverishment/IMG/Unmod/img1.jpg"))
-                {
-                    CalculatePixel(imgload);
-                }
+                List<string> audioFiles = GetAudioFiles();
+
+                // Start a new thread to play background music
+                Thread musicThread = new Thread(() => PlayRandomBackgroundMusic(audioFiles));
+                musicThread.Start();
+
+                int files = RenameFilesIncremental(directoryPath);
+                LoadPicture(files);
+
+                // Once the main logic is done, signal the music thread to stop
+                stopMusic = true;
+                musicThread.Join(); // Wait for the music thread to stop
             }
             catch (Exception ex)
             {
-                Console.WriteLine("An error occurred while loading the image: " + ex.Message);
-                Console.ReadKey();
+                Console.WriteLine($"An error occurred: {ex.Message}");
             }
         }
-        public static void CalculatePixel(Bitmap imgcalc)
+        static List<string> GetAudioFiles()
+        {
+            string[] audioFiles = Directory.GetFiles(audioFilePath, "*.mp3");
+            List<string> files = new List<string>();
+            foreach (var item in audioFiles)
+            {
+                if (!item.Contains("successAudio"))
+                {
+                    files.Add(item);
+                }
+            }
+            return files;
+        }
+        static void PlayRandomBackgroundMusic(List<string> audioFiles)
+        {
+            Random random = new Random();
+
+            while (!stopMusic)
+            {
+                string randomAudioFilePath = audioFiles[random.Next(audioFiles.Count)];
+
+                using (AudioFileReader audioFile = new AudioFileReader(randomAudioFilePath))
+                using (var outputDevice = new WaveOutEvent())
+                {
+                    outputDevice.Init(audioFile);
+                    outputDevice.Play();
+
+                    while (!stopMusic && outputDevice.PlaybackState == PlaybackState.Playing)
+                    {
+                        Thread.Sleep(1000);
+                    }
+                }
+            }
+        }
+        static int RenameFilesIncremental(string directoryPath)
+        {
+            if (!Directory.Exists(directoryPath))
+            {
+                Console.WriteLine("Directory not found.");
+            }
+            Console.WriteLine("DIRECTORY FOUND...");
+            Console.WriteLine();
+
+            string[] files = Directory.GetFiles(directoryPath);
+
+            for (int i = 0; i < files.Length; i++)
+            {
+                string oldFilePath = files[i];
+                string newFileName = $"IMG_{i + 1:D4}.jpg";
+                string newFilePath = Path.Combine(directoryPath, newFileName);
+
+                try
+                {
+                    File.Move(oldFilePath, newFilePath);
+                    Console.WriteLine($"Renamed '{Path.GetFileName(oldFilePath)}' to '{newFileName}'.");
+                    Thread.Sleep(timerFiles);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Error renaming '{Path.GetFileName(oldFilePath)}': {ex.Message}");
+                }
+            }
+
+            return files.Length;
+        }
+        public static void LoadPicture(int files)
+        {
+            for (int i = 1; i < files+1; i++)
+            {
+                string formattedCounter = String.Format("{0:D4}", i);
+                string filename = $"IMG_{formattedCounter}.jpg";
+                Console.Clear();
+                Console.WriteLine($"LOADING 'IMG_{formattedCounter}.JPG'...");
+                Thread.Sleep(timer);
+
+                try
+                {
+                    // Load image
+                    using (Bitmap imgLoad = new Bitmap(loadFilePath + filename))
+                    {
+                        Console.WriteLine("CALCULATING AVG COLOR VALUE...");
+                        Thread.Sleep(timer);
+                        double avg = Math.Round(CalculateAverage(imgLoad, filename));
+
+                        Console.Clear();
+                        Console.WriteLine($"AVERAGE R/G/B VALUE IS: {avg}");
+                        Thread.Sleep(timer);
+                        Console.ForegroundColor = ConsoleColor.White;
+
+                        Console.WriteLine("CALCULATING INDIVIDUAL PIXELS...");
+                        Thread.Sleep(timer);
+                        CalculatePixel(imgLoad, filename, avg);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.Clear();
+                    Console.WriteLine("An error occurred while loading the image: " + ex.Message);
+                    Console.ReadKey();
+                    break;
+                }
+            }
+        }
+        public static double CalculateAverage(Bitmap imgCalc, string filename)
         {
             double i = 0;
-            double max = imgcalc.Height * imgcalc.Width;
-            double cent = 0;
+            double max = imgCalc.Height * imgCalc.Width;
+            double percentage = 0;
+            double value = 0;
+            string formattedMax = (max % 100000000).ToString("00\\.000\\.000");
+
+            Console.Clear();
+
             // Loop through each pixel
-            for (int y = 0; y < imgcalc.Height; y++)
+            for (int y = 0; y < imgCalc.Height; y++)
             {
-                for (int x = 0; x < imgcalc.Width; x++)
+                for (int x = 0; x < imgCalc.Width; x++)
                 {
-                    // Get the color of the current pixel
-                    Color pixelColor = imgcalc.GetPixel(x, y);
+                    Color pixelColor = imgCalc.GetPixel(x, y);
 
-                    // Change the color of the pixel
-                    Color newColor = PickColor(pixelColor);
-
-                    // Set the new color of the pixel
-                    imgcalc.SetPixel(x, y, newColor);
+                    value += pixelColor.R + pixelColor.G + pixelColor.B;
 
                     i++;
 
-                    if(i % 10000  == 0)
+                    if (i % 100000 == 0)
                     {
-                        cent = (i / max) * 100;
-                        Console.WriteLine(i + " of " + max + " pixel processed || " + Math.Round(cent,2) + "% finished");
+                        string formattedCounter = (i % 100000000).ToString("00\\.000\\.000");
+                        string formattedPercent = (Math.Round(percentage, 1) * 100).ToString("00\\,00\\%");
+                        percentage = (i / max) * 100;
+                        Console.WriteLine("STEP 1/2 || Calculating average pixel value || " + formattedCounter + "/" + formattedMax + " pixel processed || " + formattedPercent + $" --> {filename}");
                     }
                 }
             }
 
-            SavePicture(imgcalc);
+            return (value / 3) / max;
         }
-        public static Color PickColor(Color pxcolor)
+        public static void CalculatePixel(Bitmap imgCalc, string filename, double avg)
         {
-            Color[] standardColors = new Color[]
+            double i = 0;
+            double max = imgCalc.Height * imgCalc.Width;
+            double percentage = 0;
+            string formattedMax = (max % 100000000).ToString("00\\.000\\.000");
+
+            Console.Clear();
+
+            // Loop through each pixel
+            for (int y = 0; y < imgCalc.Height; y++)
             {
-                Color.Black,
-                Color.White                
-            };
-
-            Color closestColor = standardColors[0];
-
-            int closestDistance = int.MaxValue;
-
-            foreach (Color color in standardColors)
-            {
-                int distance = CalculateColorDistance(pxcolor, color);
-
-                if (distance < closestDistance)
+                for (int x = 0; x < imgCalc.Width; x++)
                 {
-                    closestColor = color;
-                    closestDistance = distance;
+                    // Get the color of the current pixel
+                    Color pixelColor = imgCalc.GetPixel(x, y);
+
+                    // Change the color of the pixel
+                    Color newColor = PickColor(pixelColor, avg);
+
+                    // Set the new color of the pixel
+                    imgCalc.SetPixel(x, y, newColor);
+
+                    i++;
+
+                    if (i % 100000 == 0)
+                    {
+                        string formattedCounter = (i % 100000000).ToString("00\\.000\\.000");
+                        string formattedPercent = (Math.Round(percentage, 1) * 100).ToString("00\\,00\\%");
+                        percentage = (i / max) * 100;
+                        Console.WriteLine("STEP 2/2 || Calculating new pixel color || " + formattedCounter + "/" + formattedMax + " pixel processed || " + formattedPercent + $" --> {filename}");
+                    }
                 }
             }
 
-            return closestColor;
+            SavePicture(imgCalc, filename);
         }
-
-        private static int CalculateColorDistance(Color color1, Color color2)
+        public static Color PickColor(Color pxcolor, double avg)
         {
-            int redDifference = color1.R - color2.R;
-            int greenDifference = color1.G - color2.G;
-            int blueDifference = color1.B - color2.B;
+            if (pxcolor.R < avg && pxcolor.G < avg && pxcolor.B < avg)
+            {
+                return Color.Black;
+            }
 
-            return (redDifference * redDifference) + (greenDifference * greenDifference) + (blueDifference * blueDifference);
+            return Color.White;
         }
 
-        public static void SavePicture(Bitmap imgsave)
+        public static void SavePicture(Bitmap imgSave, string filename)
         {
             try
             {
                 // Save the modified image
-                imgsave.Save($"C:/Users/domrie01/Documents/PictureImpoverishment/PictureImpoverishment/IMG/Mod/mod_img.png");
+                imgSave.Save(saveFilePath + "MOD_" + filename);
             }
             catch (Exception ex)
             {
+                Console.Clear();
                 Console.WriteLine("An error occurred while saving the image: " + ex.Message);
+                Console.ReadKey();
             }
             finally
             {
-                imgsave.Dispose();
+                imgSave.Dispose();
 
                 Console.WriteLine();
-                Console.WriteLine("Image Processing finished, push any button to close.");
-                Console.ReadKey();
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine($"PROCESSING FINISHED. YOUR FILE WAS SAVED SUCCESSFULLY AS 'MOD_{filename}'!");
+
+
+                using (AudioFileReader audioFile = new AudioFileReader(audioFilePathSuccess))
+                using (var outputDevice = new WaveOutEvent())
+                {
+                    outputDevice.Init(audioFile);
+                    outputDevice.Play();
+                    while (outputDevice.PlaybackState == PlaybackState.Playing)
+                    {
+                        Thread.Sleep(3000);
+                    }
+                }
+                Console.ForegroundColor = ConsoleColor.White;
             }
         }
     }
